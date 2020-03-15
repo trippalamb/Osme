a27b8a182aeb231058fa31ac8a3d7cd53155d615
Tripp.Lamb <trippalamb@gmail.com>
2020-03-15 14:34:09 -0500

    worked on token nester

--- Language/app.test/TokenNester.test.js
+++ Language/app.test/TokenNester.test.js
@@ -1,8 +1,15 @@
 const TokenNester = require("../app/Parser/TokenNester.js");
-var containerTokens = [
-    { start: 'paren-open', end: 'paren-close', name:"sub-exp" },
-    { start: 'vector-open', end: 'vector-close', name:"matrix" },
-];
+var containerTokens = {
+    "expression": { start: 'exp-open', end: 'exp-close', name:"expression" },
+    "vector": { start: 'vector-open', end: 'vector-close', name:"vector" },
+    "tuple": { start: 'tuple-open', end: 'tuple-close', name:"tuple" },
+    "dict": { start: 'dict-open', end: 'dict-close', name:"dict" },
+    "function": {start:'fxn-open', end: 'fxn-close', name:"function"},
+    "subroutine":{start:'sub-open', end: 'sub-close', name:"subroutine"},
+    "do":{start:'do-open', end:'do-close', name:"do"},
+    "if":{start:'if-open', end:'if-close', name:"do"}
+
+};
 //left angle '?'
 //right angle '?'
 
@@ -24,17 +31,17 @@ test("TokenNester | nest() : tests '2 + 4.5'", () => {
 
 test("TokenNester | create() : tests '(2 + 4.5)'", () => {
     var tokens = [
-        { type: "punctuation", sub: "parentheses", name: "paren-open", val: "(" },
+        { type: "punctuation", sub: "expression", name: "exp-open", val: "(" },
         { type: "literal", sub: "number", name: "real", val: "2" },
         { type: "operator", sub: "add-ops", name: "add", val: "+" },
         { type: "literal", sub: "number", name: "real", val: "4.5" },
-        { type: "punctuation", sub: "parentheses", name: "paren-close", val: ")" }
+        { type: "punctuation", sub: "expression", name: "exp-close", val: ")" }
     ];
 
     var correct = [
         {
             type: "container",
-            name: "sub-exp",
+            name: "expression",
             val: [
                 { type: "literal", sub: "number", name: "real", val: "2" },
                 { type: "operator", sub: "add-ops", name: "add", val: "+" },
@@ -46,13 +53,49 @@ test("TokenNester | create() : tests '(2 + 4.5)'", () => {
     expect(TokenNester.nest(tokens, containerTokens)).toEqual(correct);
 });
 
-test("TokenNester | create() : tests <2.0,3.0,4.0>'", () => {
+test("TokenNester | create() : tests '(2.0 + (3.0 - 4.5))'", () => {
+    var tokens = [
+        { type: "punctuation", sub: "expression", name: "exp-open", val: "(" },
+        { type: "literal", sub: "number", name: "real", val: "2.0" },
+        { type: "operator", sub: "add-ops", name: "add", val: "+" },
+        { type: "punctuation", sub: "expression", name: "exp-open", val: "(" },
+        { type: "literal", sub: "number", name: "real", val: "3.0" },
+        { type: "operator", sub: "add-ops", name: "minus", val: "-" },
+        { type: "literal", sub: "number", name: "real", val: "4.5" },
+        { type: "punctuation", sub: "expression", name: "exp-close", val: ")" },
+        { type: "punctuation", sub: "expression", name: "exp-close", val: ")" }
+    ];
+
+    var correct = [
+        {
+            type: "container",
+            name: "expression",
+            val: [
+                { type: "literal", sub: "number", name: "real", val: "2.0" },
+                { type: "operator", sub: "add-ops", name: "add", val: "+" },
+                {
+                    type: "container",
+                    name: "expression",
+                    val: [
+                        { type: "literal", sub: "number", name: "real", val: "3.0" },
+                        { type: "operator", sub: "add-ops", name: "minus", val: "-" },
+                        { type: "literal", sub: "number", name: "real", val: "4.5" }
+                    ]
+                }
+            ]
+        }
+    ];
+
+    expect(TokenNester.nest(tokens, containerTokens)).toEqual(correct);
+});
+
+test("TokenNester | create() : tests '<2.0,3.0,4.0>'", () => {
     var tokens = [
         { type: "punctuation", sub: "vector", name: "vector-open", val: "<" },
         { type: "literal", sub: "number", name: "real", val: "2.0" },
-        { type: "punctuation", sub: "comma", name: "comma", val: "," },
+        { type: "punctuation", sub: "separator", name: "separator", val: "," },
         { type: "literal", sub: "number", name: "real", val: "3.0" },
-        { type: "punctuation", sub: "comma", name: "comma", val: "," },
+        { type: "punctuation", sub: "separator", name: "separator", val: "," },
         { type: "literal", sub: "number", name: "real", val: "4.0" },
         { type: "punctuation", sub: "vector", name: "vector-close", val: ">" }
     ];
@@ -60,7 +103,7 @@ test("TokenNester | create() : tests <2.0,3.0,4.0>'", () => {
     var correct = [
         {
             type: "container",
-            name: "matrix",
+            name: "vector",
             val: [
                 { type: "literal", sub: "number", name: "real", val: "2.0" },
                 { type: "literal", sub: "number", name: "real", val: "3.0" },
@@ -71,3 +114,55 @@ test("TokenNester | create() : tests <2.0,3.0,4.0>'", () => {
 
     expect(TokenNester.nest(tokens, containerTokens)).toEqual(correct);
 });
+
+test("TokenNester | create() : tests '[2.0, 'hello', true]'", () => {
+    var tokens = [
+        { type: "punctuation", sub: "tuple", name: "tuple-open", val: "[" },
+        { type: "literal", sub: "number", name: "real", val: "2.0" },
+        { type: "punctuation", sub: "separator", name: "separator", val: "," },
+        { type: "literal", sub: "text", name: "string", val: "hello" },
+        { type: "punctuation", sub: "separator", name: "separator", val: "," },
+        { type: "literal", sub: "number", name: "boolean", val: "true" },
+        { type: "punctuation", sub: "tuple", name: "tuple-close", val: "]" }
+    ];
+
+    var correct = [
+        {
+            type: "container",
+            name: "tuple",
+            val: [
+                { type: "literal", sub: "number", name: "real", val: "2.0" },
+                { type: "literal", sub: "text", name: "string", val: "hello" },
+                { type: "literal", sub: "number", name: "boolean", val: "true" }
+            ]
+        }
+    ];
+
+    expect(TokenNester.nest(tokens, containerTokens)).toEqual(correct);
+});
+
+//test("TokenNester | create() : tests '{n:2.0, msg:'hello', yes:true}'", () => {
+//    //var tokens = [
+//    //    { type: "punctuation", sub: "tuple", name: "tuple-open", val: "[" },
+//    //    { type: "literal", sub: "number", name: "real", val: "2.0" },
+//    //    { type: "punctuation", sub: "separator", name: "separator", val: "," },
+//    //    { type: "literal", sub: "text", name: "string", val: "hello" },
+//    //    { type: "punctuation", sub: "separator", name: "separator", val: "," },
+//    //    { type: "literal", sub: "number", name: "boolean", val: "true" },
+//    //    { type: "punctuation", sub: "tuple", name: "tuple-close", val: "]" }
+//    //];
+//
+//    //var correct = [
+//    //    {
+//    //        type: "container",
+//    //        name: "tuple",
+//    //        val: [
+//    //            { type: "literal", sub: "number", name: "real", val: "2.0" },
+//    //            { type: "literal", sub: "text", name: "string", val: "hello" },
+//    //            { type: "literal", sub: "number", name: "boolean", val: "true" }
+//    //        ]
+//    //    }
+//    //];
+//
+//    expect(TokenNester.nest(tokens, containerTokens)).toEqual(correct);
+//});
--- Language/app/Parser/TokenNester.js
+++ Language/app/Parser/TokenNester.js
@@ -5,23 +5,63 @@ class TokenNester {
         
         var nested = [];
 
-        while (tokens.length > 0) {
-            if (tokens[0].name === cts[0].start) {
-                var i = findMatchingIndex(tokens, cts[0].start, cts[0].end);
-                tokens.shift();
-                var nt = { //nested token
-                    type: "container",
-                    name: cts[0].name,
-                    val: tokens.splice(0, i - 1)
-                }
-                tokens.shift();
-                nested.push(nt);
-            }
-            else {
-                nested.push(tokens.shift());
-            }
-        }
+        
 
+        while (tokens.length > 0) {
+
+            var type = tokens[0].sub;
+            switch(tokens[0].sub){
+                case ("expression"):
+                    var i = findMatchingIndex(tokens, cts[type].start, cts[type].end);
+                    tokens.shift();
+                    var nt = { //nested token
+                        type: "container",
+                        name: cts[type].name,
+                        val: this.nest(tokens.splice(0, i - 1), cts)
+                    }
+                    tokens.shift();
+                    nested.push(nt);
+                    break;
+
+                case ("vector"):
+                    var i = findMatchingIndex(tokens, cts[type].start, cts[type].end);
+                    tokens.shift();
+                    var val = this.nest(tokens.splice(0, i - 1), cts)
+                    val = val.filter((v) => v.sub !== "separator");
+                    val.every((v) => v.sub === "number");
+                    var nt = { //nested token
+                        type: "container",
+                        name: cts[type].name,
+                        val: val
+                    }
+                    tokens.shift();
+                    nested.push(nt);
+                    break;
+
+                case ("tuple"):
+                    var i = findMatchingIndex(tokens, cts[type].start, cts[type].end);
+                    tokens.shift();
+                    var val = this.nest(tokens.splice(0, i - 1), cts)
+                    val = val.filter((v) => v.sub !== "separator")
+                    var nt = { //nested token
+                        type: "container",
+                        name: cts[type].name,
+                        val: val
+                    }
+                    tokens.shift();
+                    nested.push(nt);
+                    break;
+
+                case ("dict"):
+
+                    break;
+
+                default:
+                    nested.push(tokens.shift());
+
+            }
+
+        }
 
         return nested;
 
@@ -41,7 +81,7 @@ function findMatchingIndex(tokens, startVal, endVal) {
 
     while (i < tokens.length) {
         if (tokens[i].name === startVal) {
-            i += findMatchingIndex(tokens.slice(i), startVal, endVal);
+            i += findMatchingIndex(tokens.slice(i), startVal, endVal) + 1;
         }
         else if (tokens[i].name === endVal) {
             return i;
@@ -51,5 +91,5 @@ function findMatchingIndex(tokens, startVal, endVal) {
         }
     }
 
-    throw new Error("Missing closing '" + endChar + "'");
+    throw new Error("Missing closing '" + endVal + "'");
 }
